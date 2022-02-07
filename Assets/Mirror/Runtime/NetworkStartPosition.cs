06059625508051c507912faf65d06ddf5363a915
Jbumk <dhow153@gmail.com>
2022-02-07 18:43:08 +0900

    22-02-07
    네트워크 테스트
    시작위치조정 후 각 플레이어에 오브젝트 할당

--- Assets/Mirror/Runtime/NetworkStartPosition.cs
+++ Assets/Mirror/Runtime/NetworkStartPosition.cs
@@ -11,10 +11,29 @@ namespace Mirror
     public class NetworkStartPosition : MonoBehaviour
     {
         public GameObject StartPosition;
+        public GameObject Player1;
+        public GameObject Player2;
+        public GameObject Gamemanager;
+        public NetworkManager manager;
 
         public void Awake()
         {
+            Player1 = GameObject.Find("Player1");
+            Player2 = GameObject.Find("Player2");
+            Gamemanager = GameObject.Find("GameManager");
+            manager = Gamemanager.GetComponent<NetworkManager>();
+
+
             NetworkManager.RegisterStartPosition(StartPosition.transform);
+           
+
+            if (manager.ChkPlayerCode() == 1)
+            {
+                Player1.transform.SetParent(transform);
+            }else if (manager.ChkPlayerCode() == 2)
+            {
+                Player2.transform.SetParent(transform);
+            }
         }
 
         public void OnDestroy()
